45a6bc461f880653240805fc8ac2076e4ecc9987
Ohiduzzaman Shuvo <oh599627@dal.ca>
2023-06-05 15:30:54 -0300

    implemented layout component
    - used split screen as an use case
    - used react styled components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {SplitScreen} from "./design-pattern/SplitScreen";
+
+const LeftHandComponent = ({name}) => (
+    <h1 style={{backgroundColor: "green"}}>{name}!</h1>
+)
+
+const RightHandComponent = ({msg}) => (
+    <p style={{backgroundColor: "red"}}>{msg}!</p>
+)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    return (
+        <SplitScreen
+            leftWeight={1}
+            rightWeight={3}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <LeftHandComponent name={"Shuvo"}/>
+            <RightHandComponent msg={"Hello"}/>
+        </SplitScreen>
+    );
 }
 
 export default App;
