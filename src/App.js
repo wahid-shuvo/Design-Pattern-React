d27d25a97588452981e0d4a73922665d08d01b53
Ohiduzzaman Shuvo <oh599627@dal.ca>
2023-06-05 23:11:45 -0300

    updated container component
    - used data source component to read server data
    -used data source component to read local data

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import {CurrentUserLoader} from "./components/container-component/CurrentUserLoader"
-import {UserLoader} from "./components/container-component/UserLoader"
+import {CurrentUserLoader} from "./components/container-component/CurrentUserLoader";
+import {UserLoader} from "./components/container-component/UserLoader";
 import {UserInfo} from "./components/container-component/UserInfo";
 import {ProductInfo} from "./components/container-component/ProductInfo";
-import {ResourceLoader} from "./components/container-component/ResourceLoader"
+import {ResourceLoader} from "./components/container-component/ResourceLoader";
+import {DataSourceLoader} from "./components/container-component/DataSourceLoader";
+import axios from 'axios';
 
+const getServerData = url => async () => {
+    const response = await axios.get(url);
+    return response.data;
+}
+const getLocalStorageData = key => () => {
+    return localStorage.getItem(key)
+}
 
+const Text=({message})=> <h1>{message}</h1>
 
 function App() {
     return (
         <>
-            <ResourceLoader resourceUrl={"/users/123"} resourceName={"user"}>
-            <UserInfo/>
-            </ResourceLoader>
+            <DataSourceLoader getDataFunc={getServerData('users/123')} resourceName={"user"}>
+                <UserInfo/>
+            </DataSourceLoader>
 
-            <ResourceLoader resourceUrl={"/products/1234"} resourceName={"product"}>
-                <ProductInfo/>
-            </ResourceLoader>
+            <DataSourceLoader getDataFunc={getLocalStorageData('message')} resourceName={"message"}>
+                <Text/>
+            </DataSourceLoader>
 
         </>
     );
---
+++ src/components/container-component/DataSourceLoader.js
@@ -0,0 +1,28 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
+
+export const DataSourceLoader = ({
+                               getDataFunc = () => {
+                               }, resourceName, children
+                           }) => {
+    const [state, setState] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            const data = await getDataFunc();
+            setState(data)
+        })();
+    }, [getDataFunc]);
+
+    return (
+        <>
+            {React.Children.map(children, child => {
+                if (React.isValidElement(child)) {
+                    return React.cloneElement(child, {[resourceName]: state})
+                }
+                return child
+            })}
+        </>
+    )
+
+}
