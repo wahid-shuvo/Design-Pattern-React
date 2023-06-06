cc3b299556c68b4993240a7ee233de9c530a27e0
Ohiduzzaman Shuvo <oh599627@dal.ca>
2023-06-06 18:31:58 -0300

    added uncontrolled form

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
-import {CurrentUserLoader} from "./components/container-component/CurrentUserLoader";
-import {UserLoader} from "./components/container-component/UserLoader";
-import {UserInfo} from "./components/container-component/UserInfo";
-import {ProductInfo} from "./components/container-component/ProductInfo";
-import {ResourceLoader} from "./components/container-component/ResourceLoader";
-import {DataSourceLoader} from "./components/container-component/DataSourceLoader";
-import axios from 'axios';
-
-const getServerData = url => async () => {
-    const response = await axios.get(url);
-    return response.data;
-}
-const getLocalStorageData = key => () => {
-    return localStorage.getItem(key)
-}
-
-const Text=({message})=> <h1>{message}</h1>
+import {UncontrolledForm} from "./components/controlled-uncontrolled/UncontrolledForm"
 
 function App() {
     return (
         <>
-            <DataSourceLoader getDataFunc={getServerData('users/123')} resourceName={"user"}>
-                <UserInfo/>
-            </DataSourceLoader>
-
-            <DataSourceLoader getDataFunc={getLocalStorageData('message')} resourceName={"message"}>
-                <Text/>
-            </DataSourceLoader>
-
+            <UncontrolledForm/>
         </>
-    );
-
+    )
 }
 
 export default App;
