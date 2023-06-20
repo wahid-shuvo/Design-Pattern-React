3f2efef34d7d4be4eb3c58f28beb16d5d887b205
Ohiduzzaman Shuvo <oh599627@dal.ca>
2023-06-20 00:08:55 -0300

    Implemented custom useUser hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-
 import {UserInfo} from "./components/custom-hooks-pattern/UserInfo";
-import {withUser} from "./components/higher-order-components/withUser";
-
-const UserInfoWithLoader = withUser(UserInfo, '234')
 
 function App() {
 
     return (
-        <UserInfo/>
+        <>
+            <UserInfo userId="123"/>
+            <UserInfo userId="234"/>
+            <UserInfo userId="345"/>
+        </>
     );
 }
 
